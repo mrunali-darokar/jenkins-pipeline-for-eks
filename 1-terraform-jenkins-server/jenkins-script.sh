3941284c8acafac32f22685249848c4d1762a662
Mrunali Darokar <125744670+mrunali-darokar@users.noreply.github.com>
2025-01-12 20:47:17 +0530

    Update jenkins-script.sh

--- 1-terraform-jenkins-server/jenkins-script.sh
+++ 1-terraform-jenkins-server/jenkins-script.sh
@@ -2,7 +2,7 @@
 
 # install jenkins
 
-sudo yum update
+sudo yum update -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo \
     https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io-2023.key
